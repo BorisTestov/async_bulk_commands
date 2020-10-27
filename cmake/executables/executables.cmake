e91b5c48d54301e275ca7717dc7f64e7df33a2d9
boristestov <boris.testov@gmail.com>
2020-10-28 00:41:13 +0300

    libasync added to install
     Changes to be committed:
    	modified:   cmake/executables/executables.cmake

--- cmake/executables/executables.cmake
+++ cmake/executables/executables.cmake
@@ -1,11 +1,29 @@
 include_directories(${INCLUDE_DIR})
 
-file (GLOB SOURCES "${SRC_DIR}/*.cpp")
-file (GLOB HEADERS "${INCLUDE_DIR}/*.h" "${INCLUDE_DIR}/*.hpp")
+add_library(libasync SHARED
+        include/async.h
+        include/block_processor.h
+        include/command_container.h
+        include/command_processor.h
+        include/observer.h
+        include/stream.h
+        src/async.cpp
+        src/block_processor.cpp
+        src/command_container.cpp
+        src/command_processor.cpp
+        src/observer.cpp
+        src/stream.cpp
+        )
 
-add_executable(${PROJECT_NAME} ${SOURCES} ${HEADERS})
+add_executable(${PROJECT_NAME} src/main.cpp src/version.cpp include/version.h)
+target_link_libraries(${PROJECT_NAME} libasync)
 set_target_properties(${PROJECT_NAME} PROPERTIES ${TARGET_PROPERTIES})
+set_target_properties(${PROJECT_NAME} PROPERTIES PUBLIC_HEADER include/async.h)
 target_compile_options(${PROJECT_NAME} PRIVATE ${COMPILE_OPTIONS})
 
-install(TARGETS ${EXECUTABLES_LIST} RUNTIME DESTINATION bin)
+install(TARGETS libasync
+        LIBRARY DESTINATION lib
+        PUBLIC_HEADER DESTINATION include)
+
+install(FILES include/async.h DESTINATION include)
 
